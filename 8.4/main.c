7b0f2cb1ad28fe0275c94dd5d7233c87d8277c17
geekzph <geekzph@qq.com>
2016-03-06 16:01:31 +0800

    Initial Commit

---
+++ 8.4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  8.4
+//
+//  Created by zph on 16/3/6.
+//  Copyright © 2016年 zph. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
